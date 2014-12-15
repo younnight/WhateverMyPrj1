1bc9f592c993434c244fc29f7ed44112ac3ed119
younnight <younnight@gmail.com>
2014-12-15 23:47:03 +0900

    [test] needs more work
    critical bug fix..;;
    
    ,and ToDo
    algorithm must be checked.. based on log.

--- wear/src/main/java/co/kr/younnight/whatevermyprj1/WearMainActivity.java
+++ wear/src/main/java/co/kr/younnight/whatevermyprj1/WearMainActivity.java
@@ -8,6 +8,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.WindowManager;
@@ -24,9 +26,11 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 
 public class WearMainActivity extends Activity implements SensorEventListener, DataApi.DataListener,
         MessageApi.MessageListener, NodeApi.NodeListener, GoogleApiClient.ConnectionCallbacks,
@@ -43,14 +47,19 @@ public class WearMainActivity extends Activity implements SensorEventListener, D
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+	private Sensor mRotaionSensor;
 
-    private final int mAccelGestureThreshold = 7;
+    private final int mAccelGestureThreshold = 10;
 
-    private int MAX_DATA_SIZE = 20;
-    private float xValues[] = new float[MAX_DATA_SIZE];
-    private float yValues[] = new float[MAX_DATA_SIZE];
-    private float zValues[] = new float[MAX_DATA_SIZE];
-    private int xyzValuesIdx = 0;
+    private int MAX_DATA_SIZE = 30;
+    private float xAccelValues[] = new float[MAX_DATA_SIZE];
+    private float yAccelValues[] = new float[MAX_DATA_SIZE];
+    private float zAccelValues[] = new float[MAX_DATA_SIZE];
+	private float xRotaionValues[] = new float[MAX_DATA_SIZE];
+    private float yRotaionValues[] = new float[MAX_DATA_SIZE];
+    private float zRotaionValues[] = new float[MAX_DATA_SIZE];
+    private int xyzAccelValuesIdx = 0;
+	private int xyzRotaionValuesIdx = 0;
     private long mDataAddedTime = 0;
 
     private int xPositiveGesture;// 0 : on state, 1 : possible state, 2 : confirmed.
@@ -64,17 +73,18 @@ public class WearMainActivity extends Activity implements SensorEventListener, D
 
     byte gestureResult[] = new byte[6];
 
+	int fileNameIdx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.activity_wear_main);
+	    setContentView(R.layout.activity_wear_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//######################  must be called after setContentView..
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView = (TextView) stub.findViewById(R.id.mainText);
             }
         });
 
@@ -86,11 +96,13 @@ public class WearMainActivity extends Activity implements SensorEventListener, D
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+	    mRotaionSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
     }
 
     private void clearDataInfo(){
         //init data array...
-        xyzValuesIdx = 0;
+        xyzAccelValuesIdx = 0;
+	    xyzRotaionValuesIdx = 0;
 
         xPositiveGesture = 0;
         xNegativeGesture = 0;
@@ -108,82 +120,134 @@ public class WearMainActivity extends Activity implements SensorEventListener, D
         float zValue = event.values[2];
 
         if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+	        Log.e(TAG, "##AccelSensor: " + xValue + "," + yValue + "," + zValue);
 
-            if (Math.abs(xValue) > mAccelGestureThreshold || Math.abs(yValue) > mAccelGestureThreshold || Math.abs(zValue) > mAccelGestureThreshold) {
-                mTrashDataCount = 0;
+	        if (Math.abs(xValue) > mAccelGestureThreshold || Math.abs(yValue) > mAccelGestureThreshold || Math.abs(zValue) > mAccelGestureThreshold) {
+		        Log.e(TAG, "log counted as index[" + xyzAccelValuesIdx + "]");
+		        mTrashDataCount = 0;
 
-                long currentTime = System.currentTimeMillis();
-                if (mDataAddedTime != 0 && (currentTime - mDataAddedTime > 1000)) {
-                    clearDataInfo();
-                }
+		        long currentTime = System.currentTimeMillis();
+		        if (mDataAddedTime != 0 && (currentTime - mDataAddedTime > 1000)) {
+			        clearDataInfo();
+		        }
 
-                mDataAddedTime = currentTime;
+		        mDataAddedTime = currentTime;
 
-                //add data to array.
-                xValues[xyzValuesIdx] = xValue;
-                yValues[xyzValuesIdx] = yValue;
-                zValues[xyzValuesIdx++] = zValue;
-                if (xyzValuesIdx == MAX_DATA_SIZE) {
-                    //not expected.
-                    Log.e(TAG, "onSensorChanged: data overflow...............................");
-                    xyzValuesIdx--;
-                }
+		        //add data to array.
+		        xAccelValues[xyzAccelValuesIdx] = xValue;
+		        yAccelValues[xyzAccelValuesIdx] = yValue;
+		        zAccelValues[xyzAccelValuesIdx++] = zValue;
+		        if (xyzAccelValuesIdx == MAX_DATA_SIZE) {
+			        //not expected.
+			        Log.e(TAG, "onSensorChanged: data overflow...............................");
+			        xyzAccelValuesIdx--;
+		        }
 
-                //analyze data.
-                if (xValue < -mAccelGestureThreshold && xPositiveGesture == 0) {
-                    xNegativeGesture = 1;
-                }
-                if (yValue < -mAccelGestureThreshold && yPositiveGesture == 0) {
-                    yNegativeGesture = 1;
-                }
-                if (zValue < -mAccelGestureThreshold && zPositiveGesture == 0) {
-                    zNegativeGesture = 1;
-                }
-                if (xValue > mAccelGestureThreshold && xNegativeGesture == 1) {
-                    xNegativeGesture = 2;
-                    gestureResult[0] = 1;
-                }
-                if (yValue > mAccelGestureThreshold && yNegativeGesture == 1) {
-                    yNegativeGesture = 2;
-                    gestureResult[1] = 1;
-                }
-                if (zValue > mAccelGestureThreshold && zNegativeGesture == 1) {
-                    zNegativeGesture = 2;
-                    gestureResult[2] = 1;
-                }
+		        //analyze data.
+		        if (xValue < -mAccelGestureThreshold && xPositiveGesture == 0) {
+			        xNegativeGesture = 1;
+			        Log.e(TAG, "xNegativeGesture = 1");
+		        }
+		        if (yValue < -mAccelGestureThreshold && yPositiveGesture == 0) {
+			        yNegativeGesture = 1;
+			        Log.e(TAG, "yNegativeGesture = 1");
+		        }
+		        if (zValue < -mAccelGestureThreshold && zPositiveGesture == 0) {
+			        zNegativeGesture = 1;
+			        Log.e(TAG, "zNegativeGesture = 1");
+		        }
+		        if (xValue > mAccelGestureThreshold && xNegativeGesture == 1) {
+			        xNegativeGesture = 2;
+			        gestureResult[0] = 1;
+			        Log.e(TAG, "xNegativeGesture = 2");
+		        }
+		        if (yValue > mAccelGestureThreshold && yNegativeGesture == 1) {
+			        yNegativeGesture = 2;
+			        gestureResult[1] = 1;
+			        Log.e(TAG, "yNegativeGesture = 2");
+		        }
+		        if (zValue > mAccelGestureThreshold && zNegativeGesture == 1) {
+			        zNegativeGesture = 2;
+			        gestureResult[2] = 1;
+			        Log.e(TAG, "zNegativeGesture = 2");
+		        }
 
-                if (xValue > mAccelGestureThreshold && xNegativeGesture == 0) {
-                    xPositiveGesture = 1;
+		        if (xValue > mAccelGestureThreshold && xNegativeGesture == 0) {
+			        xPositiveGesture = 1;
+			        Log.e(TAG, "xPositiveGesture = 1");
+		        }
+		        if (yValue > mAccelGestureThreshold && yNegativeGesture == 0) {
+			        yPositiveGesture = 1;
+			        Log.e(TAG, "yPositiveGesture = 1");
+		        }
+		        if (zValue > mAccelGestureThreshold && zNegativeGesture == 0) {
+			        zPositiveGesture = 1;
+			        Log.e(TAG, "zPositiveGesture = 1");
+		        }
+		        if (xValue < -mAccelGestureThreshold && xPositiveGesture == 1) {
+			        xPositiveGesture = 2;
+			        gestureResult[3] = 1;
+			        Log.e(TAG, "xPositiveGesture = 2");
+		        }
+		        if (yValue < -mAccelGestureThreshold && yPositiveGesture == 1) {
+			        yPositiveGesture = 2;
+			        gestureResult[4] = 1;
+			        Log.e(TAG, "yPositiveGesture = 2");
+		        }
+		        if (zValue < -mAccelGestureThreshold && zPositiveGesture == 1) {
+			        zPositiveGesture = 2;
+			        gestureResult[5] = 1;
+			        Log.e(TAG, "zPositiveGesture = 2");
                 }
-                if (yValue > mAccelGestureThreshold && yNegativeGesture == 0) {
-                    yPositiveGesture = 1;
-                }
-                if (zValue > mAccelGestureThreshold && zNegativeGesture == 0) {
-                    zPositiveGesture = 1;
-                }
-                if (xValue < -mAccelGestureThreshold && xPositiveGesture == 1) {
-                    xPositiveGesture = 2;
-                    gestureResult[3] = 1;
-                }
-                if (yValue < -mAccelGestureThreshold && yPositiveGesture == 1) {
-                    yPositiveGesture = 2;
-                    gestureResult[4] = 1;
-                }
-                if (zValue < -mAccelGestureThreshold && zPositiveGesture == 1) {
-                    zPositiveGesture = 2;
-                    gestureResult[5] = 1;
-                }
-
             } else {
                 mTrashDataCount++;
                 if (mTrashDataCount == 10) { //check only 10 trash data counted..
-                    if (xPositiveGesture == 2 || yPositiveGesture == 2 || zPositiveGesture == 2 || xNegativeGesture == 2 || yNegativeGesture == 2 || zNegativeGesture == 2) {
+	                Log.e(TAG, "trash data............. init data");
+	                if (xPositiveGesture == 2 || yPositiveGesture == 2 || zPositiveGesture == 2 || xNegativeGesture == 2 || yNegativeGesture == 2 || zNegativeGesture == 2) {
 
-                        new SendSensorDataTask(gestureResult).execute();
-                        clearDataInfo();
+		                //write to file.
+		                try {
+			                String directory = "/sdcard/prj/";
+			                directory = Environment.getExternalStorageDirectory().toString() + "/prj/";
+			                File file = new File(directory);
+			                if(!file.exists()){
+				               if(file.mkdir()){
+					               Log.e(TAG, "created");
+				               }
+				               else{
+					               Log.e(TAG, "NOT created");
+				               }
+
+			                }
+			                file = new File(directory + "tmp" + fileNameIdx + ".txt");
+			                if(!file.exists()) {
+				                file.createNewFile();
+			                }
+			                FileWriter fw = new FileWriter(file);
+			                for(int i = 0; i < xyzRotaionValuesIdx; i++){
+				                fw.write(i + "\t" + xAccelValues[i] + "\t" + yAccelValues[i] + "\t" + zAccelValues[i] + "\t" +
+						                xRotaionValues[i] + "\t" + yRotaionValues[i] + "\t" + zRotaionValues[i] + "\n");
+			                }
+			                fw.close();
+
+							mTextView.setText("write file [" + fileNameIdx + "]");
+			                fileNameIdx++;
+		                } catch (IOException e) {
+			                e.printStackTrace();
+		                }
+
                     }
+	                clearDataInfo();
                 }
             }
+        }
+	    else if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+	        if(xyzRotaionValuesIdx < xyzAccelValuesIdx){
+		        xRotaionValues[xyzRotaionValuesIdx] = xValue;
+		        yRotaionValues[xyzRotaionValuesIdx] = yValue;
+		        zRotaionValues[xyzRotaionValuesIdx++] = zValue;
+		        //Log.e(TAG, "rotaionSensor: " + xValue + "," + yValue + "," + zValue);
+	        }
         }
 
     }
@@ -230,12 +294,15 @@ public class WearMainActivity extends Activity implements SensorEventListener, D
     @Override
     public void onResume() {
         super.onResume();
+	    Log.e(TAG, "onResume #############################################");
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+	    mSensorManager.registerListener(this, mRotaionSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
     public void onPause() {
         super.onPause();
+	    Log.e(TAG, "onPause #############################################");
         mSensorManager.unregisterListener(this);
     }
 
